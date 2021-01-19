87ec5858749441bcbde70c6571b04b1628c2cfb7
Bryan Valentini <transistorize@users.noreply.github.com>
2021-01-19 13:47:06 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Initial Social Canvas Prototype</h1>
+      <h2>Put all visualizations here</h2>
+      <div id="embed-platforms"></div>
+    </div>
+  );
+}
