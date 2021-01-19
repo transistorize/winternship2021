37a6f717c900131f0e9774747efad86b67b0c715
Bryan Valentini <transistorize@users.noreply.github.com>
2021-01-19 13:50:40 -0500

    test github integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Initial Social Canvas Prototype</h1>
-      <h2>Put all visualizations here</h2>
+      <h2>Put all visualizations here please</h2>
       <div id="embed-platforms"></div>
     </div>
   );
